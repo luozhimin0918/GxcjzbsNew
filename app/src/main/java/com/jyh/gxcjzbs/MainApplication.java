d83082331b04cf640dde1c388359dc9f14f5f071
Android-罗志敏 <54543534@domain.com>
2017-08-30 17:44:04 +0800

    成功解救support v4  重复包问题

--- app/src/main/java/com/jyh/gxcjzbs/MainApplication.java
+++ app/src/main/java/com/jyh/gxcjzbs/MainApplication.java
@@ -3,6 +3,11 @@ package com.jyh.gxcjzbs;
 import android.app.Application;
 
 import com.jyh.gxcjzbs.model.ChatMsgEntity;
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
 import com.socks.library.KLog;
 
 import java.util.List;
@@ -16,6 +21,18 @@ public class MainApplication extends Application {
     public void onCreate() {
         super.onCreate();
         KLog.init(BuildConfig.LOG_DEBUG);
+        //网络图片例子,结合常用的图片缓存库UIL,你可以根据自己需求自己换其他网络图片库
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder().
+                showImageForEmptyUri(R.mipmap.ic_default_adimage)
+                .cacheInMemory(true).cacheOnDisk(true).build();
+
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(
+                getApplicationContext()).defaultDisplayImageOptions(defaultOptions)
+                .threadPriority(Thread.NORM_PRIORITY - 2)
+                .denyCacheImageMultipleSizesInMemory()
+                .diskCacheFileNameGenerator(new Md5FileNameGenerator())
+                .tasksProcessingOrder(QueueProcessingType.LIFO).build();
+        ImageLoader.getInstance().init(config);
     }
 
 
