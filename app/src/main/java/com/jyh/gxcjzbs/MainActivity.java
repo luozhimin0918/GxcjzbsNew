49000fdf77724339ae5b85a04c22d0e876cefb2a
Android-罗志敏 <54543534@domain.com>
2017-08-31 10:18:13 +0800

    沉浸式ok

--- app/src/main/java/com/jyh/gxcjzbs/MainActivity.java
+++ app/src/main/java/com/jyh/gxcjzbs/MainActivity.java
@@ -3,6 +3,8 @@ package com.jyh.gxcjzbs;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.flyco.tablayout.CommonTabLayout;
 import com.jyh.gxcjzbs.base.BaseActivity;
@@ -19,6 +21,12 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 透明状态栏
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        // 透明导航栏
+//		getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main, StatusBarColor.NO_COLOR);
         main=this;
         mainPresenter = new MainPresenter(this);
